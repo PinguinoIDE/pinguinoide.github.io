3ce85c4679fe8b687a824ac936559a20601e4ddc
rblanchot <rblanchot@pinguino.cc>
2019-12-16 13:41:56 +0100

    Form pinguino.cc to pinguino.github.io

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include "inc/head.inc"; ?>
+
+<div id="header">
+    <!-- Pinguino Logo -->
+</div>
+
+<div id="container">
+    <h1>What Pinguino stands for</h1>
+    <div id="cell"><img src=img/icons/computer.svg><br>General-purpose electronic physical computer</div>
+    <div id="cell"><img src=img/icons/easy.svg><br>Easily programmable</div>
+    <div id="cell"><img src=img/icons/OSHW.svg><br>Open Source Hardware Environment</div>
+    <div id="cell"><img src=img/icons/chips.svg><br>Wide range of &copy;Microchip 8- and 32-bit microcontrollers</div>
+    <div id="cell"><img src=img/icons/ide.svg><br>Powerful Integrated Development Environement (IDE)</div>
+    <div id="cell"><img src=img/icons/os.svg><br>Multi-platform : 32- and 64-bit GNU/Linux, Windows and Mac OS X</div>
+    <div id="cell"><img src=img/icons/usb.svg><br>USB bootloader</div>
+    <div id="cell"><img src=img/icons/peripherals.svg><br>Multi peripherals on board (USB, Serial, SPI, I2C, ...)</div>
+    <div id="cell"><img src=img/icons/group.svg><br>Great supportive community</div>
+    <h1><a href="download.php"><img src=img/download.png width=30%></a></h1>
+</div>
+
+<?php include "inc/footer.inc"; ?>
