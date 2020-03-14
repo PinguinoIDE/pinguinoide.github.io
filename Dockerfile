dcd4013b6c0b87520062a4c80d9baba9b22e45ca
Victor Villarreal <mefhigoseth@gmail.com>
2020-03-14 12:22:08 -0300

    feat(core): Enabled Google Analytics

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM node:12-alpine
 WORKDIR /usr/src
-RUN yarn global add vuepress babel-loader
+RUN yarn global add vuepress babel-loader @vuepress/plugin-google-analytics
--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,4 +1,12 @@
 module.exports = {
   title: 'Pinguino project',
-  description: 'Physical computing for everyone'
+  description: 'Physical computing for everyone',
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'UA-160403552-1' // UA-00000000-0
+      }
+    ]
+  ]
 }
