be878a6e4246dda9ba145c0270d82ec939233c75
Victor Villarreal <mefhigoseth@gmail.com>
2020-04-19 02:34:11 -0300

    feat(i18n): Agregado idioma es-AR a la configuración del sitio

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -1,6 +1,18 @@
 module.exports = {
   title: 'Pinguino project',
   description: 'Physical computing for everyone',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Pinguino project',
+      description: 'Physical computing for everyone',
+    },
+    '/es/': {
+      lang: 'es-AR',
+      title: 'Proyecto Pinguino',
+      description: 'Computación física para todos',
+    }
+  },
   plugins: [
     [
       '@vuepress/google-analytics',
